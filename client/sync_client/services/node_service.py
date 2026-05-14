e90818fa89e9e9ac2871dc4fa1a5c6ec112fe53e
codefritzel <pascal-pf@hotmail.de>
2026-05-14 18:15:19 +0200

    expand sync node service
    added evacuate_node, migrate_node &
    modify_node

--- client/sync_client/services/node_service.py
+++ client/sync_client/services/node_service.py
@@ -1,3 +1,6 @@
+import typing
+from typing import Any, Optional
+
 from client.models.node import Node
 from client.sync_client.api_client import RAPIClient
 from client.utils import dict_to_dataclass
@@ -17,3 +20,46 @@ class NodeService:
         node_info_raw = self.api_client.get(f"{self.ENDPOINT}/{node_name}")
         node_info_raw = {key.replace(".", "_"): value for key, value in node_info_raw.items()}
         return dict_to_dataclass(Node, node_info_raw)
+
+    def evacuate_node(
+        self,
+        node_name: str,
+        early_release: bool = False,
+        iallocator: Optional[str] = None,
+        ignore_soft_errors: bool = False,
+        mode: Optional[str] = None,
+        remote_node: Optional[str] = None,
+    ) -> int:
+        return typing.cast(
+            int,
+            self.api_client.post(
+                f"{self.ENDPOINT}/{node_name}/evacuate",
+                early_release=early_release,
+                iallocator=iallocator,
+                ignore_soft_errors=ignore_soft_errors,
+                mode=mode,
+                remote_node=remote_node,
+            ),
+        )
+
+    def migrate_node(
+        self,
+        node_name: str,
+        allow_runtime_changes: bool = True,
+        iallocator: Optional[str] = None,
+        mode: Optional[str] = None,
+        target_node: Optional[str] = None,
+    ) -> int:
+        return typing.cast(
+            int,
+            self.api_client.post(
+                f"{self.ENDPOINT}/{node_name}/migrate",
+                allow_runtime_changes=allow_runtime_changes,
+                iallocator=iallocator,
+                mode=mode,
+                target_node=target_node,
+            ),
+        )
+
+    def modify_node(self, node_name: str, **kwargs: Any) -> int:
+        return typing.cast(int, self.api_client.post(f"{self.ENDPOINT}/{node_name}/modify", **kwargs))
--- tests/services/test_node_service.py
+++ tests/services/test_node_service.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Any, Callable, Dict
 from unittest.mock import MagicMock
 
 import pytest
@@ -48,3 +48,47 @@ class TestNodeService:
         assert node.name == "node1.example.com"
         assert node.pip == "192.168.1.200"
         assert node.group_uuid == "4d3bf3ba-972e-49b0-8680-f783fb07a048"
+
+    def test_evacuate_node(
+        self,
+        node_service: NodeService,
+        mock_http_client: MagicMock,
+        mock_response: Callable[[Any, int], MagicMock],
+    ) -> None:
+        mock_http_client.request.return_value = mock_response(123, 200)
+
+        job_id = node_service.evacuate_node("node1.example.com")
+        call_args = mock_http_client.request.call_args
+        assert job_id == 123
+        assert call_args[0][0] == "POST"
+        assert "node1.example.com/evacuate" in call_args[0][1]
+
+    def test_migrate_node(
+        self,
+        node_service: NodeService,
+        mock_http_client: MagicMock,
+        mock_response: Callable[[Any, int], MagicMock],
+    ) -> None:
+        mock_http_client.request.return_value = mock_response(123, 200)
+
+        job_id = node_service.migrate_node("node1.example.com")
+        call_args = mock_http_client.request.call_args
+        assert job_id == 123
+        assert call_args[0][0] == "POST"
+        assert "node1.example.com/migrate" in call_args[0][1]
+
+    def test_modify_node(
+        self,
+        node_service: NodeService,
+        mock_http_client: MagicMock,
+        mock_response: Callable[[Any, int], MagicMock],
+    ) -> None:
+        mock_http_client.request.return_value = mock_response(123, 200)
+
+        job_id = node_service.modify_node("node1.example.com", offline=True)
+        call_args = mock_http_client.request.call_args
+        json: Dict[str, Any] = call_args.kwargs["json"]
+        assert job_id == 123
+        assert call_args[0][0] == "POST"
+        assert "node1.example.com/modify" in call_args[0][1]
+        assert "offline" in json and json["offline"]
